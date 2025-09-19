c63b5111ed2d6affe73a74dd71104f932eea41b2
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 19:49:12 -0300

    react

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 
+import FirstComponent from './componets/Firstcomponent';
+
 function App() {
-  return <div className='App'></div>
+  return <div className='App'>
+    <h1>Fundamentos do React</h1>
+    <FirstComponent />
+  </div>
 }
 
 export default App;

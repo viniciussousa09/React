0631ffdf20b00b47d40726acdc03f21858ba3a63
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 19:23:39 -0300

    React

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 2 - importando componente
+import FirstComponent from "./components/FirstComponent";
+
+// 4 - template expression
+import TemplateExpression from "./components/TemplateExpression";
+
+// 5 - hierarquia de componentes
+import MyComponent from "./components/MyComponent";
+
+// 6 - eventos
+import Events from "./components/Events";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return 
-    <div className='App'>
-
+  // 3 - Comentários na função ou fora
+  return (
+    <div className="App">
+      {/* 3 - Comentários no componente */}
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
+      <TemplateExpression />
+      <MyComponent />
+      <Events />
     </div>
+  );
 }
 
-export default App
+export default App;

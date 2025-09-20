c98426d7f7fc9ea9526ff4fa2561eade67460260
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 22:00:18 -0300

    Introdução Hook

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -3,14 +3,19 @@ import "./App.css";
 // 2 - imagem em assets
 import night from './assets/night.jpg';
 
+// 3 - useState
+import Data from "./components/Data";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ paddigBottom: "500px" }}>
       <h1>Avançando em React</h1>
       {/* 1 - Imagem em public */}
       <img src="/img.jpg" alt="Alguma imagem" />
       {/* 2 - Imagem em assets */}
       <img src={night} alt="Outra imagem" />
+      {/* 3 - useState */}
+      <Data />
     </div>
   );
 }

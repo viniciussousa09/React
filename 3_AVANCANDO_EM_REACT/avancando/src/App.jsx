e546f5ee2d891d54d0dc636915e27547dfea8105
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 21:41:04 -0300

    Importando imagens

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import "./App.css";
+
+// 2 - imagem em assets
+import night from './assets/night.jpg';
 
 function App() {
   return (
-  <div className="App">
-<h1>Avançando em React</h1>
-{/* 1 - Imagem em public */}
-<img src="/img.jpg" alt="Alguma imagem" />
-  </div>
-  )
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* 1 - Imagem em public */}
+      <img src="/img.jpg" alt="Alguma imagem" />
+      {/* 2 - Imagem em assets */}
+      <img src={night} alt="Outra imagem" />
+    </div>
+  );
 }
 
-export default App
+export default App;

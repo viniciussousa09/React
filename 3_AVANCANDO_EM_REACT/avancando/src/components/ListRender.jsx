bee32a97645ad2ad61f0155bbe625cef9aea5a30
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 22:22:36 -0300

    Propriedade Key

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -3,13 +3,30 @@ import { useState } from "react";
 const ListRender = () => {
     const [list] = useState(["Vinicius", "Lais", "Giovanna", "Sophia", "Alice", "Daniel"]);
 
-    return (<div>
-        <ul>
-            {list.map((item) => (
-                <li>{item}</li>
-            ))}
-        </ul>
-    </div>
+    const [users, setUsers] = useState([
+        { id: 1, name: "Vinicius", age: 39 },
+        { id: 2, name: "Lais", age: 38 },
+        { id: 3, name: "Giovanna", age: 16 },
+        { id: 4, name: "Sophia", age: 15 },
+        { id: 5, name: "Alice", age: 10 },
+        { id: 6, name: "Daniel", age: 6 },
+    ]);
+
+    return (
+        <div>
+            {/* 4 - render sem Key */}
+            <ul>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            {/* 5 - render com key */}
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age} anos</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 

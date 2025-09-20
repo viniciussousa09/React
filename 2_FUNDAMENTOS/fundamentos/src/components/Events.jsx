cd3562dd74540a53b21378b3aeb3e5a762da4b8b
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 21:19:42 -0300

    Função de render

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -6,6 +6,17 @@ const Events = () => {
         console.log("Executou");
     };
 
+    // 8 - Função e renderização
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso!</h1>;
+        } else {
+            return <h1>Renderizando outra coisa!</h1>;
+        }
+    };
+
+   // return 10 > 2 && <p>Carregando...</p>;
+
     return (
         <div>
             <div>
@@ -15,6 +26,9 @@ const Events = () => {
             <div>
                 <button onClick={handleClick}>Clique aqui - com função</button>
             </div>
+            {/* 8 - Função com render */}
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     );
 };

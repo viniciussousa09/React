f8ab63671da17c4f257427d2b9f762321001e571
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-19 21:33:48 -0300

    Inserindo imagem

---
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+  <div className="App">
+<h1>Avançando em React</h1>
+{/* 1 - Imagem em public */}
+<img src="/img.jpg" alt="Alguma imagem" />
+  </div>
+  )
+}
+
+export default App

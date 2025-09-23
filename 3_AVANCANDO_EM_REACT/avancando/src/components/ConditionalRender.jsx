8cf4f2608954bdf1ad12acc86b620dab37c40bd1
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-23 20:02:27 -0300

    Adicionando else

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
@@ -3,11 +3,22 @@ import React from "react";
 const ConditionalRender = () => {
     const x = 5;
 
+    const name = "João";
+
     return (
         <div>
             {/* render condicional */}
             <h3>Isso será exibido?</h3>
             {x > 2 && <p>Se x for true sim!</p>}
+            {/* 8 - Else */}
+            <h3>Render ternário</h3>
+            {name === "João" ? (
+                <div>
+                    <p>Olá João!</p>
+                </div>
+            ) : (
+                <p>Nome não encontrado!</p>
+            )}
         </div>
     );
 };

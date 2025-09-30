5daf44900b690ab0e17ba8662b1d69dc5dba136c
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-29 21:10:33 -0300

    Criando Form em React

---
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -0,0 +1,16 @@
+import "./MyForm.css";
+
+const MyForm = () => {
+    return <div>
+        {/* 1 - criação de form */}
+        <form>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="name" placeholder="Digite seu nome" />
+            </div>
+            <input type="submit" value="Enviar" />
+        </form>
+    </div>;
+};
+
+export default MyForm;

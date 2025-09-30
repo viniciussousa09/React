d52b06816bdc39fb46aced494e02c49131a8132f
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-29 22:45:39 -0300

    Criando componentes

---
+++ PROJETO_IMC_CALC/imc_calc/src/App.jsx
@@ -0,0 +1,13 @@
+import ImcCalc from "./components/imcCalc";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <ImcCalc />
+    </div>
+  );
+}
+
+export default App;

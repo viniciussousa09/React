7ab36412160a9617a4b2b2da31289094015549d6
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-09-29 21:16:08 -0300

    Label envolvendo input

--- 5_FORM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -8,6 +8,11 @@ const MyForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite seu nome" />
             </div>
+            {/* 2 - label envolvendo input */}
+            <label>
+                <span>E-mail:</span>
+                <input type="text" name="email" placeholder="Digite seu e-mail" />
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>;

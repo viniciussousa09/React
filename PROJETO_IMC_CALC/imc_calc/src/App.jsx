132e27e51e345f671aca6206220e8890a2ed15a4
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-02 22:52:23 -0300

    estruturando componente

--- PROJETO_IMC_CALC/imc_calc/src/App.jsx
+++ PROJETO_IMC_CALC/imc_calc/src/App.jsx
@@ -11,15 +11,25 @@ import "./App.css";
 function App() {
   const calcImc = (e, height, weight) => {
     e.preventDefault();
-    console.log("Executou");
+
+    if (!weight || !height) return;
+
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
+
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+    setImc(imcResult);
   };
 
   const [imc, setImc] = useState("");
   const [info, setInfo] = useState("");
   const [infoClass, setInfoClass] = useState("");
 
-  return <div className="container">{!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
-  </div>;
+  return (
+  <div className="container">{!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable data={data} />}
+  </div>
+  );
 }
 
 export default App;

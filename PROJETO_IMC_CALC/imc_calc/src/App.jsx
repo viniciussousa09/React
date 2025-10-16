6e7ecb6311177ae00829a7b596fa9a5a84df2904
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-16 20:45:41 -0300

    Funcionalidades, e CSS

--- PROJETO_IMC_CALC/imc_calc/src/App.jsx
+++ PROJETO_IMC_CALC/imc_calc/src/App.jsx
@@ -20,6 +20,23 @@ function App() {
     const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
 
     setImc(imcResult);
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setInfoClass(item.infoClass);
+      }
+    });
+
+    if (!info) return;
+  };
+
+  const resetCalc = (e) => {
+    e.preventDefault();
+
+    setImc("");
+    setInfo("");
+    setInfoClass("");
   };
 
   const [imc, setImc] = useState("");
@@ -27,8 +44,10 @@ function App() {
   const [infoClass, setInfoClass] = useState("");
 
   return (
-  <div className="container">{!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable data={data} />}
-  </div>
+    <div className="container">{!imc ? <ImcCalc calcImc={calcImc} /> : (
+      <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc} />
+    )}
+    </div>
   );
 }
 
--- PROJETO_IMC_CALC/imc_calc/src/components/ImcTable.jsx
+++ PROJETO_IMC_CALC/imc_calc/src/components/ImcTable.jsx
@@ -1,28 +1,34 @@
-import React from "react";
 import Button from "./Button";
 
-const ImcTable = ({data}) => {
-    return <div id="result-container">
-        <p id="imc-number">Seu Imc:</p>
-        <p id="imc-info">Situação atual:</p>
-        <h3>Confira as classificações:</h3>
-        <div id="imc-table">
-            <div className="table-header">
-                <h4>IMC</h4>
-                <h4>Clacificação</h4>
-                <h4>Obesidade</h4>
-            </div>
-            {data.map((item) => (
-                <div className="table-data" key={item.info}>
-                    <p>{item.classification}</p>
-                    <p>{item.info}</p>
-                    <p>{item.obesity}</p>
-                </div>
-            ))}
+import "./ImcTable.css";
+
+const ImcTable = ({ data, imc, info, resetCalc, infoClass }) => {
+  return (
+    <div id="result-container">
+      <p id="imc-number">
+        Seu IMC: <span className={infoClass}>{imc}</span>
+      </p>
+      <p id="imc-info">
+        Situação atual: <span className={infoClass}>{info}</span>
+      </p>
+      <h3>Confira as classificações</h3>
+      <div id="imc-table">
+        <div className="table-header">
+          <h4>IMC</h4>
+          <h4>Classificação</h4>
+          <h4>Obesidade</h4>
         </div>
-        <Button id="back-btn" text="Voltar" />
+        {data.map((item) => (
+          <div className="table-data" key={item.info}>
+            <p>{item.classification}</p>
+            <p>{item.info}</p>
+            <p>{item.obesity}</p>
+          </div>
+        ))}
+      </div>
+      <Button id="back-btn" text="Voltar" action={(e) => resetCalc(e)} />
     </div>
-    
+  );
 };
 
 export default ImcTable;

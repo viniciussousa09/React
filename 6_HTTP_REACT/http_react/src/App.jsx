231e54cebefa6b4e0c98a188741fecc521e07434
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-17 20:29:40 -0300

    Resgate de dados

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -1,10 +1,32 @@
-import './App.css';
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
+import "./App.css";
 
 function App() {
- 
+  // 1 - resgatando dados
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <h1>HTTP em React</h1>
+      {/* 1 - resgate de dados */}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R${product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }

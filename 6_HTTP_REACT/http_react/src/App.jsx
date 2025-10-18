33a9e7a94ee1f75e5737e04a10bfabcfe41276bc
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-17 21:16:44 -0300

    Envio de dados

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -18,6 +18,27 @@ function App() {
     getData();
   }, []);
 
+  // 2 - envio de dados
+  const [name, setname] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+  };
+
   return (
     <div className="App">
       <h1>HTTP em React</h1>
@@ -27,6 +48,20 @@ function App() {
           <li key={product.id}>{product.name} - R${product.price}</li>
         ))}
       </ul>
+      {/* 2 - enviando dados */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome</span>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            <span>Preço</span>
+            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
     </div>
   );
 }

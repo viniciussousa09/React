b2665731744689a290796ef9e82eec31bac328a4
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-17 23:14:15 -0300

    tratando erro

--- 6_HTTP_REACT/http_react/src/App.jsx
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import { useFetch } from "./hooks/useFetch";
 
-const url = "http://localhost:3000/products";
+const url = "";
 
 import "./App.css";
 
@@ -11,79 +11,81 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - custom hook
-  const { data: items, httpConfig, loading } = useFetch(url);
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
+  // useEffect(() => {
+  //   async function getData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
 
-     getData();
-     }, []);
+  //   getData();
+  // }, []);
 
-    // 2 - envio de dados
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
+  // 2 - envio de dados
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-      const product = {
-        name,
-        price,
-      };
-
-      // 5 - refatorando post
-      httpConfig(product, "POST");
-
-      // const res = await fetch(url, {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json"
-      //   },
-      //   body: JSON.stringify(product),
-      // });
-
-      // // 3 - carregamento dinamico
-      // const addedProduct = await res.json();
-
-      // setProducts((prevProducts) => [...prevProducts, addedProduct]);
-
+    const product = {
+      name,
+      price,
     };
 
-    return (
-      <div className="App">
-        <h1>HTTP em React</h1>
-        {/* 6 - loading */}
-        {loading && <p>Carregando...</p>}
-        {/* 1 - resgate de dados */}
-        <ul>
-          {items &&
-            items.map((product) => (
-              <li key={product.id}>{product.name} - R${product.price}</li>
-            ))}
-        </ul>
-        {/* 2 - enviando dados */}
-        <div className="add-product">
-          <form onSubmit={handleSubmit}>
-            <label>
-              <span>Nome</span>
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <label>
-              <span>Preço</span>
-              <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
-            </label>
-            {/* <input type="submit" value="Enviar" /> */}
-            {/* 7 - loading post */}
-            {loading && <input type="submit" disabled value="Aguarde" />}
-            {!loading && <input type="submit" value="Criar" />}
-          </form>
-        </div>
+    // 5 - refatorando post
+    httpConfig(product, "POST");
+
+    //  const res = await fetch(url, {
+    //    method: "POST",
+    //    headers: {
+    //      "Content-Type": "application/json"
+    //    },
+    //    body: JSON.stringify(product),
+    //  });
+
+    //  // 3 - carregamento dinamico
+    //  const addedProduct = await res.json();
+
+    //  setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+  };
+
+  return (
+    <div className="App">
+      <h1>HTTP em React</h1>
+      {/* 6 - loading */}
+      {loading && <p>Carregando...</p>}
+      {/* 7 - tratando erro */}
+      {error && <p>{error}</p>}
+      {/* 1 - resgate de dados */}
+      <ul>
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>{product.name} - R${product.price}</li>
+          ))}
+      </ul>
+      {/* 2 - enviando dados */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome</span>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            <span>Preço</span>
+            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          {/* <input type="submit" value="Enviar" /> */}
+          {/* 7 - loading post */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default App;
--- 6_HTTP_REACT/http_react/src/hooks/useFetch.jsx
+++ 6_HTTP_REACT/http_react/src/hooks/useFetch.jsx
@@ -12,6 +12,9 @@ export const useFetch = (url) => {
     // 6 - loading
     const [loading, setLoading] = useState(false);
 
+    // 7 - erros
+    const [error, setError] = useState(null);
+
     const httpConfig = (data, method) => {
         if (method === "POST") {
             setConfig((
@@ -27,15 +30,26 @@ export const useFetch = (url) => {
 
     useEffect(() => {
         const fetchData = async () => {
-            // 6 - loading
-            setLoading(true);
-            const res = await fetch(url);
-            const json = await res.json();
+            // 7 - tratando eros
+            try {
+                // 6 - loading
+                setLoading(true);
+
+                const res = await fetch(url);
+                const json = await res.json();
+
+
+
+                setData(json);
+            } catch (error) {
+                console.log(error.message);
+
+                setError("Houve algum erro ao carregar os dados!");
+            }
 
             setLoading(false);
-
-            setData(json);
         };
+
         fetchData();
     }, [url, callFetch]);
 
@@ -63,5 +77,5 @@ export const useFetch = (url) => {
         httpRequest();
     }, [config, method, url]);
 
-    return { data, httpConfig, loading };
+    return { data, httpConfig, loading, error };
 };

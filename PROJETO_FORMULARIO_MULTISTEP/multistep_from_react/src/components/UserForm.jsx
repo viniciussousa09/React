7cf2879a74619fc985d52f370c283cf2256a6204
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-27 21:16:25 -0300

    Estilos

--- PROJETO_FORMULARIO_MULTISTEP/multistep_from_react/src/components/UserForm.jsx
+++ PROJETO_FORMULARIO_MULTISTEP/multistep_from_react/src/components/UserForm.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const UserForm = () => {
     return (
         <div>
-            <h2>User form</h2>
+            <div className="form-control">
+                <label htmlFor="name">Nome: </label>
+                <input type="text" name="name" id="name" placeholder="Digite o seu nome" required />
+            </div>
+            <div className="form-control">
+                <label htmlFor="email">E-mail: </label>
+                <input type="email" name="email" id="email" placeholder="Digite o seu e-mail" required />
+            </div>
         </div>
     );
 };

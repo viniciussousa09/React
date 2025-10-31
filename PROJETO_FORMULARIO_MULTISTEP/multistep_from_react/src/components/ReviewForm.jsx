73a8923061c20d1075b31a78e774e07ee472a502
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-10-31 19:48:35 -0300

    Componente de avaliação

--- PROJETO_FORMULARIO_MULTISTEP/multistep_from_react/src/components/ReviewForm.jsx
+++ PROJETO_FORMULARIO_MULTISTEP/multistep_from_react/src/components/ReviewForm.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import {
+    BsFillEmojiHeartEyesFill,
+    BsFillEmojiSmileFill,
+    BsFillEmojiNeutralFill,
+    BsFillEmojiFrownFill,
+} from 'react-icons/bs';
+
+import "./ReviewForm.css";
 
 const ReviewForm = () => {
     return (
         <div className='review-form'>
             <div className="form-control score-container">
                 <label className='radio-container'>
-                    <input type="radio" value="unsatisfeito" name='review' required />
+                    <input type="radio" value="unsatisfied" name="review" required />
+                    <BsFillEmojiFrownFill />
                     <p>Insatisfeito</p>
                 </label>
+                <label className='radio-container'>
+                    <input type="radio" value="neutral" name="review" required />
+                    <BsFillEmojiNeutralFill />
+                    <p>Poderia ser melhor</p>
+                </label>
+                <label className='radio-container'>
+                    <input type="radio" value="satisfied" name="review" required />
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfeito</p>
+                </label>
+                <label className='radio-container'>
+                    <input type="radio" value="very_satisfied" name="review" required />
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito satisfeito</p>
+                </label>
+            </div>
+            <div className="form-control">
+                <label htmlFor="comment">Comentário:</label>
+                <textarea name="commnet" id="comment" placeholder="Conte como foi a sua experiência como produto..." required></textarea>
             </div>
         </div>
     );

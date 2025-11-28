4f84728d06772d333440e88ab97b50c920e9d90e
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-11-28 00:04:08 -0300

    Alterando o Estado

--- 8_QUIZ_EM_REACT/quiz/src/App.jsx
+++ 8_QUIZ_EM_REACT/quiz/src/App.jsx
@@ -1,15 +1,21 @@
 // react, componentes, estáticos
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz";
 
 import Welcome from './components/Welcome';
+import Questions from './components/Question';
 
 import './App.css';
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
 
   return (
     <div className='App'>
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Questions />}
     </div>
   );
 }

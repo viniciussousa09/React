0858a5e3fb6f6c6aed13283bc05ffa054b9a5e5b
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-11-27 23:29:02 -0300

    Criando contexto

--- 8_QUIZ_EM_REACT/quiz/src/main.jsx
+++ 8_QUIZ_EM_REACT/quiz/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+import { QuizProvider } from './context/quiz.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </StrictMode>,
-)
+);

973888e377694b27f4516f45ca67881aa0119c89
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-12-01 22:59:50 -0300

    Chegando ao fim do jogo

--- 8_QUIZ_EM_REACT/quiz/src/App.jsx
+++ 8_QUIZ_EM_REACT/quiz/src/App.jsx
@@ -4,6 +4,7 @@ import { QuizContext } from "./context/quiz";
 
 import Welcome from './components/Welcome';
 import Questions from './components/Question';
+import GameOver from "./components/GameOver";
 
 import './App.css';
 
@@ -20,6 +21,7 @@ function App() {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Questions />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
--- 8_QUIZ_EM_REACT/quiz/src/context/quiz.jsx
+++ 8_QUIZ_EM_REACT/quiz/src/context/quiz.jsx
@@ -30,10 +30,16 @@ const QuizReducer = (state, action) => {
 
         case "CHANGE_QUESTION":
             const nexQuestion = state.currentQuestion + 1;
+            let endGame = false;
+
+            if (!questions[nexQuestion]) {
+                endGame = true;
+            }
 
             return {
                 ...state,
                 currentQuestion: nexQuestion,
+                gameStage: endGame ? STAGES[2] : state.gameStage,
             };
 
         default:

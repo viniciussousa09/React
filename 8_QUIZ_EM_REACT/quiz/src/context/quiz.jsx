48683e41a6110ef1742543189405005a3928307e
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-12-12 20:39:16 -0300

    Funcionalidades

--- 8_QUIZ_EM_REACT/quiz/src/context/quiz.jsx
+++ 8_QUIZ_EM_REACT/quiz/src/context/quiz.jsx
@@ -1,102 +1,132 @@
 import { createContext, useReducer } from "react";
-import questions from '../data/questions_complete';
+import questions from "../data/questions_complete";
 
 const STAGES = ["Start", "Category", "Playing", "End"];
 
 const initialState = {
-    gameStage: STAGES[0],
-    questions,
-    currentQuestion: 0,
-    score: 0,
-    answerSelected: false,
-    help: false
+  gameStage: STAGES[0],
+  questions,
+  currentQuestion: 0,
+  answerSelected: false,
+  score: 0,
+  help: false,
+  optionToHide: null,
 };
 
-const QuizReducer = (state, action) => {
+console.log(initialState);
 
+const quizReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_STAGE":
+      return {
+        ...state,
+        gameStage: STAGES[1],
+      };
 
-    switch (action.type) {
-        case "CHANGE_STATE":
-            return {
-                ...state,
-                gameStage: STAGES[1],
-            };
+    case "START_GAME":
+      let quizQuestions = null;
 
-        case "START_GAME":
-            let quizQuestions = null;
+      state.questions.forEach((question) => {
+        if (question.category === action.payload) {
+          quizQuestions = question.questions;
+        }
+      });
 
-            state.questions.forEach((question) => {
-                if (question.category === action.payload) {
-                    quizQuestions = question.questions;
-                }
-            });
+      return {
+        ...state,
+        questions: quizQuestions,
+        gameStage: STAGES[2],
+      };
 
-            return {
-                ...state,
-                questions: quizQuestions,
-                gameStage: STAGES[2],
-            };
+    case "REORDER_QUESTIONS":
+      const reorderedQuestions = state.questions.sort(() => {
+        return Math.random() - 0.5;
+      });
 
-        case "REORDER_QUESTIONS":
-            const reorderQuestions = state.questions.sort(() => {
-                return Math.random() - 0.5;
-            });
+      return {
+        ...state,
+        questions: reorderedQuestions,
+      };
 
-            return {
-                ...state,
-                questions: reorderQuestions,
-            };
+    case "CHANGE_QUESTION": {
+      const nextQuestion = state.currentQuestion + 1;
+      let endGame = false;
 
-        case "CHANGE_QUESTION":
-            const nexQuestion = state.currentQuestion + 1;
-            let endGame = false;
+      if (!state.questions[nextQuestion]) {
+        endGame = true;
+      }
 
-            if (!state.questions[nexQuestion]) {
-                endGame = true;
-            }
-
-            return {
-                ...state,
-                currentQuestion: nexQuestion,
-                gameStage: endGame ? STAGES[3] : state.gameStage,
-                answerSelected: false,
-                help: false,
-            };
-
-        case "NEW_GAME":
-            return initialState;
-
-        case "CHECK_ANSWER":
-            if (state.answerSelected) return state;
-
-            const answer = action.payload.answer;
-            const option = action.payload.option;
-            let correctAnswer = 0;
-
-            if (answer === option) correctAnswer = 1;
-
-            return {
-                ...state,
-                score: state.score + correctAnswer,
-                answerSelected: option,
-            };
-
-        case "SHOW_TIP":
-            return {
-                ...state,
-                help: "tip",
-            };
-
-
-        default:
-            return state;
+      return {
+        ...state,
+        currentQuestion: nextQuestion,
+        gameStage: endGame ? STAGES[3] : state.gameStage,
+        answerSelected: false,
+        help: false,
+      };
     }
+
+    case "NEW_GAME": {
+      console.log(questions);
+      console.log(initialState);
+      return initialState;
+    }
+
+    case "CHECK_ANSWER": {
+      if (state.answerSelected) return state;
+
+      const answer = action.payload.answer;
+      const option = action.payload.option;
+      let correctAnswer = 0;
+
+      if (answer === option) correctAnswer = 1;
+
+      return {
+        ...state,
+        score: state.score + correctAnswer,
+        answerSelected: option,
+      };
+    }
+
+    case "SHOW_TIP": {
+      return {
+        ...state,
+        help: "tip",
+      };
+    }
+
+    case "REMOVE_OPTION": {
+      const questionWithoutOption = state.questions[state.currentQuestion];
+
+      console.log(state.currentQuestion);
+
+      console.log(questionWithoutOption);
+
+      let repeat = true;
+      let optionToHide;
+
+      questionWithoutOption.options.forEach((option) => {
+        if (option !== questionWithoutOption.answer && repeat) {
+          optionToHide = option;
+          repeat = false;
+        }
+      });
+
+      return {
+        ...state,
+        optionToHide,
+        help: true,
+      };
+    }
+
+    default:
+      return state;
+  }
 };
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-    const value = useReducer(QuizReducer, initialState);
+  const value = useReducer(quizReducer, initialState);
 
-    return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };

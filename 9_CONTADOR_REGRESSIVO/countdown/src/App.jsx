e0b744eb072d7657dd8075cee4d3f1ba96cc83d3
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-12-12 22:10:45 -0300

    Criando lógica

--- 9_CONTADOR_REGRESSIVO/countdown/src/App.jsx
+++ 9_CONTADOR_REGRESSIVO/countdown/src/App.jsx
@@ -3,18 +3,22 @@ import Counter from './components/Counter';
 
 import NewYear from "./assets/newyear.jpg";
 
+import useCountdown from './hooks/useCountdown';
+
 import './App.css';
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("Jan 1, 2026 00:00:00");
+
   return (
     <div className='App' style={{ backgroundImage: `url(${NewYear})` }}>
       <div className="container">
         <Title title="Contagem regressiva para 2026" />
         <div className="countdown-container">
-          <Counter title="Dias" number={2} />
-          <Counter title="Horas" number={2} />
-          <Counter title="Minutos" number={2} />
-          <Counter title="Segundos" number={2} />
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
         </div>
       </div>
     </div>

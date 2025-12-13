0dc4ddaa5752b2a54126b5814277121daabcb6dd
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-12-12 21:18:01 -0300

    Criando componentes

---
+++ 9_CONTADOR_REGRESSIVO/countdown/src/App.jsx
@@ -0,0 +1,24 @@
+import Title from './components/Title';
+import Counter from './components/Counter';
+
+import NewYear from "./assets/newyear.jpg";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App' style={{ backgroundImage: `url(${NewYear})` }}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2026" />
+        <div className="contdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

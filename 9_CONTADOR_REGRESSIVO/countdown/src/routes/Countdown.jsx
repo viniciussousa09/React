69e8ac5553a1ab59041e2d943bc4905f66aeeb6b
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-12-13 21:49:43 -0300

    Consumindo o contexto

--- 9_CONTADOR_REGRESSIVO/countdown/src/routes/Countdown.jsx
+++ 9_CONTADOR_REGRESSIVO/countdown/src/routes/Countdown.jsx
@@ -1,13 +1,28 @@
 import Title from '../components/Title';
 import Counter from '../components/Counter';
+import { Navigate } from 'react-router-dom';
+
+import { useContext } from 'react';
 
 import useCountdown from '../hooks/useCountdown';
 
+import { CountdownContext } from '../context/CountdownContext';
+
 const Countdown = () => {
-    const [day, hour, minute, second] = useCountdown("Jan 1, 2026 00:00:00");
+    const { event } = useContext(CountdownContext);
+
+    if (!event) return <Navigate to="/" />;
+
+    console.log(event);
+
+    const eventTitle = event.title;
+
+    const eventColor = event.color;
+
+    const [day, hour, minute, second] = useCountdown(event.date);
     return (
         <>
-            <Title title="Contagem regressiva para 2026" />
+            <Title title={eventTitle} />
             <div className="countdown-container">
                 <Counter title="Dias" number={day} />
                 <Counter title="Horas" number={hour} />

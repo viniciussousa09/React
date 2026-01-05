d268099c70418acd11a1d56bb002c396bbb6ef5a
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-01-05 18:59:18 -0300

    useReducer

--- 11_HOOKS/reacthooks/src/routes/Home.jsx
+++ 11_HOOKS/reacthooks/src/routes/Home.jsx
@@ -1,13 +1,12 @@
+import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 
-
 const Home = () => {
-
-
     return (
         <div>
             <h1>Home</h1>
             <HookUseState />
+            <HookUseReducer />
         </div>
     );
 };

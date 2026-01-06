0003b112fa07d223fc160dc89eacf9e4d9cb243c
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-01-06 20:40:02 -0300

    Custom hooks

--- 11_HOOKS/reacthooks/src/routes/Home.jsx
+++ 11_HOOKS/reacthooks/src/routes/Home.jsx
@@ -9,6 +9,7 @@ import HookUseCallback from "../components/HookUseCallback";
 import HookUseMemo from "../components/HookUseMemo";
 import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 import HookUseImperativeHandle from "../components/HookUseImperativeHandle";
+import HookCustom from "../components/HookCustom";
 
 const Home = () => {
     const { contextValue } = useContext(Somecontext);
@@ -27,6 +28,7 @@ const Home = () => {
             <HookUseMemo />
             <HookUseLayoutEffect />
             <HookUseImperativeHandle />
+            <HookCustom />
         </div>
     );
 };

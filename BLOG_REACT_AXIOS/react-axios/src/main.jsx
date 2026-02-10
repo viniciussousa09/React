05705967ee7f30a3008ec3c43cc7b50a8a6ea73d
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-09 21:32:53 -0300

    Configurando router

---
+++ BLOG_REACT_AXIOS/react-axios/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+// páginas
+import Home from './routes/Home.jsx';
+import NewPost from './routes/NewPost.jsx';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);

e98516e6c878afdf8d33bb0bdaa712bc4766a0fd
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-09 22:30:54 -0300

    exibindo post

--- BLOG_REACT_AXIOS/react-axios/src/routes/Home.jsx
+++ BLOG_REACT_AXIOS/react-axios/src/routes/Home.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+
+import { Link } from 'react-router-dom';
+
+import "./Home.css";
 
 const Home = () => {
-    return <div>Home</div>;
+
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = async () => {
+
+        try {
+
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+            const data = response.data;
+
+            setPosts(data);
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    return (
+    <div>
+        <h1>Últimos posts</h1>
+        {posts.length === 0 ? <p>Carregando...</p> : (
+            posts.map((post) => (
+                <div className="post" key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+                </div>
+            ))
+        )}
+    </div>
+    );
 };
 
 export default Home;

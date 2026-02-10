dde193c6cc50754cb867e5d3d618b88607713a57
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-09 22:42:54 -0300

    CSS da Home

--- BLOG_REACT_AXIOS/react-axios/src/routes/Home.jsx
+++ BLOG_REACT_AXIOS/react-axios/src/routes/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
     }, []);
 
     return (
-    <div>
+    <div className="home">
         <h1>Últimos posts</h1>
         {posts.length === 0 ? <p>Carregando...</p> : (
             posts.map((post) => (

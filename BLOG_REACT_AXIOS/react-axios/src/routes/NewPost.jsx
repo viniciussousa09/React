923fe07719e3a6958750ca0fbf5df6d0bf6e03ea
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-10 22:33:46 -0300

    Requisição de Post

--- BLOG_REACT_AXIOS/react-axios/src/routes/NewPost.jsx
+++ BLOG_REACT_AXIOS/react-axios/src/routes/NewPost.jsx
@@ -1,20 +1,45 @@
+import blogFetch from "../axios/config";
+
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
 import "./NewPost.css";
 
 const NewPost = () => {
-    return <div className='new-post'>
-        <h2>Inserir novo Post:</h2>
-        <form>
-            <div className="form-control">
-                <label htmlFor="title">Título:</label>
-                <input type="text" name='title' id='title' placeholder='Digite o título' />
-            </div>
-            <div className="form-control">
-                <label htmlFor="body">Conteúdo:</label>
-                <textarea name="body" id="body" placeholder='Digite o conteúdo'></textarea>
-            </div>
-            <input type="submit" value="Criar Post" className='btn' />
-        </form>
-    </div>;
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState();
+    const [body, setBody] = useState();
+
+    const creatPost = async (e) => {
+        e.preventDefault();
+
+        const post = { title, body, userId: 1 };
+
+        await blogFetch.post("/posts", {
+            body: post,
+        });
+
+        navigate("/");
+    };
+
+    return (
+        <div className='new-post'>
+            <h2>Inserir novo Post:</h2>
+            <form onSubmit={(e) => creatPost(e)}>
+                <div className="form-control">
+                    <label htmlFor="title">Título:</label>
+                    <input type="text" name='title' id='title' placeholder='Digite o título' onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="body">Conteúdo:</label>
+                    <textarea name="body" id="body" placeholder='Digite o conteúdo' onChange={(e) => setBody(e.target.value)}></textarea>
+                </div>
+                <input type="submit" value="Criar Post" className='btn' />
+            </form>
+        </div>
+    );
 };
 
 export default NewPost;

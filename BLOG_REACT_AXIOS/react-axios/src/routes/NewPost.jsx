9b31d8ed798012ef14c6d37b9f83ec45f1e3c39b
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-10 22:13:38 -0300

    New post

--- BLOG_REACT_AXIOS/react-axios/src/routes/NewPost.jsx
+++ BLOG_REACT_AXIOS/react-axios/src/routes/NewPost.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import "./NewPost.css";
 
 const NewPost = () => {
-    return <div>NewPost</div>;
+    return <div className='new-post'>
+        <h2>Inserir novo Post:</h2>
+        <form>
+            <div className="form-control">
+                <label htmlFor="title">Título:</label>
+                <input type="text" name='title' id='title' placeholder='Digite o título' />
+            </div>
+            <div className="form-control">
+                <label htmlFor="body">Conteúdo:</label>
+                <textarea name="body" id="body" placeholder='Digite o conteúdo'></textarea>
+            </div>
+            <input type="submit" value="Criar Post" className='btn' />
+        </form>
+    </div>;
 };
 
 export default NewPost;

f7957f0f8fb08396d4ce0e6f33d5c7e97a3b56c5
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-10 23:29:42 -0300

    Deletando registro

--- BLOG_REACT_AXIOS/react-axios/src/routes/Admin.jsx
+++ BLOG_REACT_AXIOS/react-axios/src/routes/Admin.jsx
@@ -21,24 +21,33 @@ const Admin = () => {
     }
   };
 
+  const deletePost = async (id) => {
+
+    await blogFetch.delete(`/posts/${id}`);
+
+    const filteredPosts = posts.filter((post) => post.id !== id);
+
+    setPosts(filteredPosts);
+  };
+
   useEffect(() => {
     getPosts();
   }, []);
 
   return <div className="admin">
     <h1>Gerenciar Posts</h1>
-    {posts.length === 0 ? (<p>Carregando...</p> ) : (
+    {posts.length === 0 ? (<p>Carregando...</p>) : (
       posts.map((post) => (
         <div className="post" key={post.id}>
           <h2>{post.title}</h2>
           <div className="actions">
             <Link className="btn edit-btn">Editar</Link>
-            <button className="btn delete-btn">Excluir</button>
+            <button className="btn delete-btn" onClick={() => deletePost(post.id)}>Excluir</button>
           </div>
         </div>
       ))
     )}
-  </div>
+  </div>;
 };
 
 export default Admin;

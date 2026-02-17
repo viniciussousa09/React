5fd8f725457b4226fb5eb108eb6b9f2d331ffd07
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-17 11:25:16 -0300

    Excluindo festa

--- PARTY-TIME/backend/controllers/partyController.js
+++ PARTY-TIME/backend/controllers/partyController.js
@@ -54,13 +54,27 @@ const partycontroller = {
 
             if (!party) {
                 res.status(404).json({ msg: "Festa não encontrada." });
+                return;
             }
 
             res.json(party);
         } catch (error) {
             console.log(error);
         }
-    }
+    },
+    delete: async (req, res) => {
+        const id = req.params.id;
+        const party = await PartyModel.findById(id);
+
+        if (!party) {
+            res.status(404).json({ msg: "Festa não encontrada." });
+            return;
+        }
+
+        const deletedParty = await PartyModel.findByIdAndDelete(id);
+
+        res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso!" });
+    },
 };
 
 module.exports = partycontroller;

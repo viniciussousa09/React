942dd7a00c2013d27558bc70b9ec079e28e129b7
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-17 11:09:44 -0300

    Encontrando festa individual

--- PARTY-TIME/backend/controllers/partyController.js
+++ PARTY-TIME/backend/controllers/partyController.js
@@ -45,6 +45,21 @@ const partycontroller = {
         } catch (error) {
             console.log(error);
         }
+    },
+    get: async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            const party = await PartyModel.findById(id);
+
+            if (!party) {
+                res.status(404).json({ msg: "Festa não encontrada." });
+            }
+
+            res.json(party);
+        } catch (error) {
+            console.log(error);
+        }
     }
 };
 
--- PARTY-TIME/backend/routes/parties.js
+++ PARTY-TIME/backend/routes/parties.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
+const partycontroller = require("../controllers/partyController");
 const partyController = require("../controllers/partyController");
 
 router.route("/parties").post((req, res) => partyController.create(req, res));
 
 router.route("/parties").get((req, res) => partyController.getAll(req, res));
+
+router.route("/parties/:id").get((req, res) => partyController.get(req, res));
 
 module.exports = router;

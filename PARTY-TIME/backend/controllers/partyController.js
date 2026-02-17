6935698ada00ea12cf287f3bbc3506714542e87b
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-17 10:35:44 -0300

    Criando festas

---
+++ PARTY-TIME/backend/controllers/partyController.js
@@ -0,0 +1,42 @@
+const PartyModel = require("../models/Party");
+
+const checkPartyBudget = (budget, services) => {
+
+    const priceSum = services.reduce((sum, service) => sum + service.price, 0);
+
+    if (priceSum > budget) {
+        return false;
+    }
+
+    return true;
+};
+
+const partycontroller = {
+    create: async (req, res) => {
+        try {
+
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services,
+            };
+
+            // BUDGET <VALOR DE SERVICOS != NOVO SERVICO
+            if (party.services && !checkPartyBudget(party.budget, party.services)) {
+                res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+                return;
+            }
+
+            const response = await PartyModel.create(party);
+
+            res.status(201).json({ response, msg: "Festa criada com sucesso." });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+module.exports = partycontroller;
--- PARTY-TIME/backend/routes/router.js
+++ PARTY-TIME/backend/routes/router.js
@@ -5,4 +5,9 @@ const serviceRouter = require("./services");
 
 router.use("/", serviceRouter);
 
+// Parties routes
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
+
 module.exports = router;

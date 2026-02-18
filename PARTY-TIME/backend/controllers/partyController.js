81cbbadce8065cc89e43314a7e72221504a7a5de
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-18 19:50:09 -0300

    Atualizando

--- PARTY-TIME/backend/controllers/partyController.js
+++ PARTY-TIME/backend/controllers/partyController.js
@@ -14,7 +14,6 @@ const checkPartyBudget = (budget, services) => {
 const partycontroller = {
     create: async (req, res) => {
         try {
-
             const party = {
                 title: req.body.title,
                 author: req.body.author,
@@ -74,6 +73,38 @@ const partycontroller = {
         const deletedParty = await PartyModel.findByIdAndDelete(id);
 
         res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso!" });
+    },
+    update: async (req, res) => {
+
+        try {
+
+            const id = req.params.id;
+
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services,
+            };
+
+            if (party.services && !checkPartyBudget(party.budget, party.services)) {
+                res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+                return;
+            }
+
+            const updateParty = await PartyModel.findByIdAndUpdate(id, party);
+
+            if (!updateParty) {
+                res.status(404).json({ msg: "Festa não encontrada." });
+                return;
+            }
+
+            res.status(200).json({ party, msg: "Festa atualizada com sucesso." });
+        } catch (error) {
+            console.log(error);
+        }
     },
 };
 
--- PARTY-TIME/backend/routes/parties.js
+++ PARTY-TIME/backend/routes/parties.js
@@ -11,4 +11,6 @@ router.route("/parties/:id").get((req, res) => partyController.get(req, res));
 
 router.route("/parties/:id").delete((req, res) => partyController.delete(req, res));
 
+router.route("/parties/:id").put((req, res) => partycontroller.update(req, res));
+
 module.exports = router;

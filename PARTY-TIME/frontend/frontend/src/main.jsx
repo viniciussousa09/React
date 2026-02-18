780120efc18976495b153764567cbf2c234f0abb
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2026-02-18 20:48:36 -0300

    Setup do frontend

---
+++ PARTY-TIME/frontend/frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages
+import Home from './routes/Home.jsx';
+import CreateParty from './routes/CreateParty.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/party/new",
+        element: <CreateParty />
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
